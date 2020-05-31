f85203cc1e57069136f88a36d42c62d7a1cd5be9
Dono <erik.dono@gmail.com>
2020-05-31 13:41:16 -0400

    added main SQL info

---
+++ db/schema.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS pt_portal_dev_DB;
+CREATE DATABASE  pt_portal_dev_DB;
+USE pt_portal_dev_DB;
+
+CREATE TABLE USER
+(
+  id INT AUTO_INCREMENT NOT NULL, 
+  first_name VARCHAR (30),
+  last_name VARCHAR (30),
+  Email VARCHAR (30),
+  user_password VARCHAR (30),
+  Trainer BOOLEAN DEFAULT true,
+  Trainer_ID INT,
+  PRIMARY KEY
+  (id)
+  
+);
+
+  CREATE TABLE Clients
+  (
+    id INT AUTO_INCREMENT NOT NULL,
+	Age INT,
+	Gender VARCHAR (30),
+	User_Weight INT,
+	User_Height INT,
+	phone_number INT,
+	Goals VARCHAR (30),
+	Injuries VARCHAR (30),
+	Medical_Conditions VARCHAR (30),
+	Diet VARCHAR (30),
+	History VARCHAR (30),
+	Session_Notes VARCHAR (30),
+	Plan_type INT,
+	user_id INT  
+);
+
+    CREATE TABLE Client_log
+    (
+      client_id INT,
+      session_note VARCHAR (30)
+    );
+
+    CREATE TABLE Plans
+    (
+      id INT AUTO_INCREMENT NOT NULL,
+      Plan_name VARCHAR (30),
+      Workouts VARCHAR (30)
+      )
