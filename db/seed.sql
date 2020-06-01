62108b890889f1df01edd6fc908fe7ef9478160e
Dono <erik.dono@gmail.com>
2020-05-31 21:09:59 -0400

    user added

--- db/seed.sql
+++ db/seed.sql
@@ -13,8 +13,14 @@ insert into Plans (Plan_name, Workouts) values ('libero quis orci', 'penatibus e
 
 insert into Client_log (client_id, Session_note) values (1, 'guy needs more help');
 
+insert into USER (first_name, last_name, email, user_password, trainer, Trainer_ID) values ('Johan', 'Kiezler', 'jkiezler0@icio.us', 'RYvrmfUK', false, 1);
+insert into USER (first_name, last_name, email, user_password, trainer, Trainer_ID) values ('Rourke', 'Gres', 'rgres1@uiuc.edu', 'uOOTYq', true, 2);
+insert into USER (first_name, last_name, email, user_password, trainer, Trainer_ID) values ('Mala', 'Wallbanks', 'mwallbanks2@jimdo.com', 'pzs482', false, 3);
+insert into USER (first_name, last_name, email, user_password, trainer, Trainer_ID) values ('Pippy', 'Watsham', 'pwatsham3@bbc.co.uk', 'd25MxPIPC', true, 4);
+insert into USER (first_name, last_name, email, user_password, trainer, Trainer_ID) values ('Wandis', 'Fiddiman', 'wfiddiman4@phoca.cz', 'J3KLkgnP', false, 5);
 
 
 select * from Clients;
 select * from Plans;
 select * from Client_log;
+select * from USER;
