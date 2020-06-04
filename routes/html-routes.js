d534c875444fbb60c84337c7dc75b39cc0458880
Brian Levin <bml201095@gmail.com>
2020-06-04 19:25:11 -0400

    changed the syntax of the html routes so it can fimd the edit client handle and render the object properly

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
+var db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -17,9 +18,12 @@ module.exports = function (app) {
   });
 
   app.get("/client/:id", function (req, res) {
-    // this code could be incorrect.. 
-    db.Clients.readAll({}).then((res) => {
-      res.render("edit-client", { client: res });
+    var id = req.params.id;
+    // this code could be incorrect..  //find(({id}))
+    db.Client.findOne({
+      where: { id: id }
+    }).then((client) => {
+      res.render("edit-client", { client }); // get one client by id instead of all
     })
     // we will need to pass a second parameter eventually
 
