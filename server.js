de31c99764ecfbea31595003705289309b53c97c
Dono <erik.dono@gmail.com>
2020-06-11 00:31:56 -0400

    force true

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ require("./routes/api-router.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function () {
+db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
     });
